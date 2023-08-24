6a4c7f71a60cca2c68210906f6b06f2a17173580
Ajha <Abhishek.jha@pentair.com>
2023-08-24 16:47:09 +0530

    Update employee.controller.js

--- employee-app/app/controllers/employee.controller.js
+++ employee-app/app/controllers/employee.controller.js
@@ -86,7 +86,7 @@ exports.update = (req, res) => {
     })
     .catch(err => {
       res.status(500).send({
-        message: "Error updating Tutorial with id=" + id
+        message: "Error updating employee with id=" + id
       });
     });
 };
